b7acc0aaa2c58997beb0939d09a9b886f43c8401
Ray Hsieh <ray@sentri.me>
2016-05-10 16:09:28 +0800

    Revert "Add log for popup window monitoring"
    This reverts commit 2f8a58add1677f99c91619d2744ed8349926d516.

--- src/actions/oauth-sign-in.js
+++ src/actions/oauth-sign-in.js
@@ -30,7 +30,6 @@ function listenForCredentials (endpointKey, popup, provider, resolve, reject) {
 
     try {
       creds = getAllParams(popup.location);
-      console.log('listenForCredentials', popup.location, creds);
     } catch (err) {}
 
     if (creds && creds.uid) {
